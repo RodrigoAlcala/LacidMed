69367bab76325e02940ac998a1f71f175e6b8d0b
RodrigoAlcala <rodrigoalcala00@yahoo.com>
2024-01-24 12:30:30 -0300

    initial visualization commit

---
+++ LacidMed/src/LacidMed/visualization/plotter.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pydicom
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+from typing import List
+
+class DicomPlotter:
+    def __init__(self, dicom_files: List[str]) -> None:
+        self.dicom_files = dicom_files
+
+    def _show_image(self, arr: np.ndarray, show: bool = True):
+        """
+        Plot a 2D or 3D array using SimpleITK.
+
+        Args:
+            arr (np.ndarray): A 2D or 3D array.
+
+        Returns:
+            matplotlib.image.AxesImage: The plot object representing the image.
+        """
+        plt_images = []
+        itk_image = sitk.GetImageFromArray(arr)
+        plt_images.append(plt.imshow(sitk.GetArrayViewFromImage(itk_image), cmap="gray"))
+        print(plt_images)
+        if show:
+            plt.show()
+        return plt_images
+
+    def plot_all_files(self):
+        def load_and_plot_images():
+            for dicom in self.dicom_files:
+                ds = pydicom.dcmread(dicom)
+                yield ds.pixel_array
+
+        for pixel_array in load_and_plot_images():
+            self._show_image(pixel_array)
+
+    def plot_single_file(self, img_num_input: int):
+        ds = pydicom.dcmread(self.dicom_files[img_num_input])
+        self._show_image(ds.pixel_array)
