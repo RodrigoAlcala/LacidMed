22c198bc38bc5f3c9e7126e743a8e5aa8e3a5936
RodrigoAlcala <rodrigoalcala00@yahoo.com>
2024-02-14 14:14:16 -0300

    filters module implementation

---
+++ lacid_med/src/processing/filters.py
@@ -0,0 +1,111 @@
+import os
+import pydicom
+from typing import List
+import SimpleITK as sitk
+
+
+class Filters:
+    def __init__(self, sequence_directory: str, dicom_files: List[pydicom.Dataset]):
+        """
+        Initialize MyClass object.
+        Args:
+            sequence_directory (str): The directory path for the sequence.
+            dicom_files (List[pydicom.Dataset]): List of DICOM files.
+        Raises:
+            ValueError: If `sequence_directory` is not a valid directory path.
+            ValueError: If `dicom_files` is an empty list.
+            ValueError: If any item in `dicom_files` is not an instance of `pydicom.Dataset`.
+            TypeError: If `sequence_directory` is not a string.
+            TypeError: If `dicom_files` is not a list.
+        """
+        if not isinstance(sequence_directory, str):
+            raise TypeError("sequence_directory must be a string")
+        if not os.path.isdir(sequence_directory):
+            raise ValueError("Invalid directory path: {}".format(sequence_directory))
+        if not isinstance(dicom_files, list):
+            raise TypeError("`dicom_files` must be a list.")
+        if not dicom_files:
+            raise ValueError("dicom_files cannot be empty")
+        for file in dicom_files:
+            if not isinstance(file, pydicom.Dataset):
+                raise ValueError(
+                    "All elements in `dicom_files` must be instances of `pydicom.Dataset`."
+                )
+        self.dicom_files = dicom_files
+        self.sequence_directory = sequence_directory
+
+    def resample_image(self, itk_image, new_spacing=[1.0, 1.0, 1.0], is_label=False):
+        """
+        Resample the input image to a new spacing.
+
+        Args:
+            itk_image: The input image to be resampled.
+            new_spacing: The new spacing to be applied.
+            is_label: Whether the image is a label image or not.
+
+        Returns:
+            The resampled image.
+        """
+        original_spacing = itk_image.GetSpacing()
+        original_size = itk_image.GetSize()
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(original_size, original_spacing, new_spacing)
+        ]
+        resampler = sitk.ResampleImageFilter()
+        resampler.SetOutputSpacing(new_spacing)
+        resampler.SetSize(new_size)
+        resampler.SetOutputDirection(itk_image.GetDirection())
+        resampler.SetOutputOrigin(itk_image.GetOrigin())
+        resampler.SetTransform(sitk.Transform())
+        resampler.SetDefaultPixelValue(itk_image.GetPixelIDValue())
+
+        if is_label:
+            resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+        else:
+            resampler.SetInterpolator(sitk.sitkBSpline)
+
+        return resampler.Execute(itk_image)
+
+    def N4_bias_correction_filter(
+        self, mask_image=None, convergence_threshold=0.001, max_iterations=[3, 3, 3]
+    ):
+        """
+        Apply N4 bias correction to the input image.
+
+        Args:
+            mask_image: The mask image to be used for bias correction.
+            convergence_threshold: The convergence threshold for the bias correction algorithm.
+            max_iterations: The maximum number of iterations for the bias correction algorithm.
+
+        Returns:
+            The bias-corrected image.
+        """
+        reader = sitk.ImageSeriesReader()
+        reader.SetFileNames(reader.GetGDCMSeriesFileNames(self.sequence_directory))
+        image = reader.Execute()
+        image = sitk.Cast(image, sitk.sitkFloat32)
+
+        image = self.resample_image(image)
+
+        corrector = sitk.N4BiasFieldCorrectionImageFilter()
+        corrector.SetMaximumNumberOfIterations(max_iterations)
+        corrector.SetConvergenceThreshold(convergence_threshold)
+
+        if mask_image is not None:
+            mask = sitk.ReadImage(mask_image)
+            n4_filtered_image = corrector.Execute(image, mask)
+        else:
+            mask = sitk.OtsuThreshold(image, 0, 1, 200)
+            n4_filtered_image = corrector.Execute(image, mask)
+
+        log_bias_field = corrector.GetLogBiasFieldAsImage(image)
+        corrected_image_full_res = n4_filtered_image / sitk.Exp(log_bias_field)
+
+        sitk.WriteImage(
+            corrected_image_full_res,
+            os.path.join(self.sequence_directory, "n4_corrected.nrrd"),
+        )
+
+        return corrected_image_full_res
