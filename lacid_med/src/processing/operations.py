0e3c86ec43bc7cf22bb6600a075eebd7ce899dfe
Santi <sbirnieoneplus@gmail.com>
2024-02-26 11:53:34 -0300

    modificaciones en operations

--- lacid_med/src/processing/operations.py
+++ lacid_med/src/processing/operations.py
@@ -6,23 +6,45 @@ import pydicom
 
 
 class Operations:
-    def __init__(self, input_directory: str = None, output_directory: str = None):
+    def __init__(
+        self, 
+        input_directory: str = None, 
+        output_directory: str = None
+    ):
+        
+        """
+        Initializes operations object.
+        Args:
+            input_directory (str): Path to the input directory.
+            output_directory (str): Path to the output directory.
+        """
+
+        if not isinstance(input_directory, str):
+            raise TypeError("input_directory must be a string")
+        if not os.path.isdir(input_directory):
+            raise ValueError("Invalid directory path: {}".format(input_directory))
+        if not os.path.exists(output_directory) or not os.access(output_directory, os.W_OK):
+            raise ValueError("Output directory does not exist or is not writable")
+
         self.input_directory = input_directory
         self.output_directory = output_directory
     
 
     def imageDiff(self, inputPath1, inputPath2):  
-        image1 = pydicom.dcmread(inputPath1) # path de la imagen original.
-        image2 = pydicom.dcmread(inputPath2) # path de la imagen procesada.
+        """
+        Substract two images and return the difference in pixel values.
+
+        Args:
+            inputPath1 (str): Path to the first image.
+            inputPath2 (str): Path to the second image.
+
+        Returns:
+            The difference in pixel values between the two images.
+        
+        """
+        image1 = pydicom.dcmread(inputPath1) # path de la imagen 1.
+        image2 = pydicom.dcmread(inputPath2) # path de la imagen 2.
         pixelArrayDiff = image2.pixel_array - image1.pixel_array
-        pixelArrayDiff[pixelArrayDiff<0] = 0
-        plt.subplot(131) 
-        plt.title('Imagen original')
-        plt.imshow(image1.pixel_array)
-        plt.subplot(132) 
-        plt.title('Imagen procesada')
-        plt.imshow(image2.pixel_array)
-        plt.subplot(133)
-        plt.title('Diferencia entre imágenes')
-        plt.imshow(pixelArrayDiff) 
-        plt.show()
+        pixelArrayDiff = np.abs(pixelArrayDiff)
+        return pixelArrayDiff
+        
