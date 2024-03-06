cc003eb861ebc8beb410e4e8b36514935eceb3f9
Santi <sbirnieoneplus@gmail.com>
2024-03-06 13:57:00 -0300

    Modificaciones en operations

--- tests/operations_tests.py
+++ tests/operations_tests.py
@@ -8,35 +8,45 @@ sys.path.append("C:/Users/santi/Desktop/Fuesmen/code/LacidMed")
 
 from lacid_med.src.processing.operations import Operations
 from lacid_med.src.visualization.plotter import DicomPlotter
+from lacid_med.src.handler.loader import DicomLoaderMRI
+from lacid_med.src.handler.writer import MultipleFileWriter
 
 def main():
+
+    # test using two paths pointing to one image each.
     path1 = "C:/Users/santi/Desktop/Fuesmen/imagenes/ZTE_H01_descomp/MR.1.2.840.113619.2.363.10499743.3637646.25026.1684513445.974.90.dcm"
+    image_1 = pydicom.dcmread(path1)
+    image_1_array = image_1.pixel_array
     path2 = "C:/Users/santi/Desktop/Fuesmen/imagenes/correction_N4_original/1.2.840.113619.2.363.10499743.3637646.25026.1684513445.974.90.dcm"
-
-    # carga de los archivos en formato dicom y conversion a array.
-    image1 = pydicom.dcmread(path1)
-    image2 = pydicom.dcmread(path2)
-    image1_array = image1.pixel_array
-    image2_array = image2.pixel_array
-
-    path_list = [path1, path2]
-
-    plotter = DicomPlotter(path_list)
-    operations = Operations()
-
-    diff = operations.imageDiff(path2, path1, clipping=True)
-    plotter._show_image(diff)
+    image_2 = pydicom.dcmread(path2)
+    image_2_array = image_2.pixel_array
+    paths = [path1, path2]
     
-
+    operations_1 = Operations(image_1_array, image_2_array)
     
-
+    plotter_1 = DicomPlotter(paths)
+    plotter_1.plot_all_files()
+    diff = operations_1.image_difference(image_2_array, image_1_array, clipping=True)
+    plotter_1._show_image(diff)
     
-
     
+    # test using two directories.
+    directory_1 = "C:/Users/santi/Desktop/Fuesmen/imagenes/ZTE_H01_descomp"
+    loader_1 = DicomLoaderMRI(directory_1)
+    vol_array_1 = loader_1.generate_volumetric_array()
 
+    directory_2 = "C:/Users/santi/Desktop/Fuesmen/imagenes/N4_volume"
+    loader_2 = DicomLoaderMRI(directory_2)
+    vol_array_2 = loader_2.generate_volumetric_array()
     
+    plotter_2 = DicomPlotter(directory_1)
 
-
+    operations_2 = Operations(vol_array_2, vol_array_1)
+    
+    volume_diff = operations_2.volume_difference(image_number=89, clipping=True)
+    
+    plotter_2._show_image(volume_diff)
+    
 
 
 if __name__ == "__main__":
