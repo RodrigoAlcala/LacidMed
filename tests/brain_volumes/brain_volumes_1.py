7b96d3ba19824f21e6c9a58f4e0e7aa6bc965b47
Santi <sbirnieoneplus@gmail.com>
2024-09-04 12:39:33 -0300

    pablo

---
+++ tests/brain_volumes/brain_volumes_1.py
@@ -0,0 +1,40 @@
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import time
+from skimage import data  
+from PIL import Image
+from scipy.ndimage import label
+from skimage import data, img_as_float
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
+from skimage.draw import circle_perimeter
+
+# lacid_med directory append.
+import sys
+sys.path.append("C:/Users/santi/Desktop/Fuesmen/code/LacidMed")
+from lacid_med.src.handler.loader import DicomLoaderMRI
+from lacid_med.src.handler.writer import SingleFileWriter, MultipleFileWriter, Deanonymizer
+from lacid_med.src.processing.filters import Filters
+from lacid_med.src.visualization.plotter import DicomPlotter
+from lacid_med.src.visualization.histograms import HistogramGenerator
+from lacid_med.src.processing.fitting import Fitting
+from lacid_med.src.processing.operations import Operations
+from lacid_med.src.processing.segmentation import Segmentation
+
+
+
+def main():
+    # -------------------------------------
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
